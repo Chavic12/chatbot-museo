6952297886d0059d4ea10fe95c4d0d7f472b25b4
Xavier Chávez <xavierchavez916@gmail.com>
2024-08-31 11:01:22 -0500

    Configuraciones

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonfy
+from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+db = SQLAlchemy(app)
+client = Groq(api_key=os.getenv('GROQ_API_KEY'))
+df = pd.read_excel('Data.xlsx')
+data = df.to_string(index=False)
+
+
 
 @app.route("/")
 @app.route("/index")
